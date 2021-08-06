44c3c53b0f4a0af167a7ad5071d94ce7fac78ee6
KianAshrafganjouei <kashr085@uottawa.ca>
2021-08-06 01:25:51 -0400

    prepared semantic UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,27 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import { isConditionalExpression } from 'typescript';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Container, Header, List } from "semantic-ui-react";
+
 function App() {
-  const[activities,setActivities]=useState([]);
-  useEffect(()=>{
-    axios.get('http://localhost:5001/api/activities').then(response=>{
-      console.log(response.data)
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5001/api/activities").then((response) => {
+      console.log(response.data);
       setActivities(response.data);
-    })
-  },[])
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-      <ul>
-        {activities.map((activity:any)=>(
-        <li key={activity.id}>
-          {activity.title}
-        </li>
+    <div>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
-      </ul>
-      </header>
+      </List>
     </div>
   );
 }
